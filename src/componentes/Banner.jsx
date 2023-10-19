882425647026d0f87b39277ab80f7061e8a5345e
KeiviVeizan <kveizanp@est.emi.edu.bo>
2023-10-19 11:30:42 -0400

    segundo componente

---
+++ src/componentes/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='Banner'>
+        <img src="./public/Banner.jpg" alt="Banner" />
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/componentes/Tarjeta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './Banner'
+import Avatar from './Avatar'
+
+function Tarjeta() {
+  return (
+    <div>
+        <Banner/>
+        <Avatar/>
+    </div>
+  )
+}
+
+export default Tarjeta
